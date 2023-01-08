48340f9aa9ddd91577a87b2568b16b72f6dfcf7d
projectperfect <jprfct@protonmail.com>
2023-01-08 05:40:48 -0800

    accepting user input

--- main.c
+++ main.c
@@ -2,5 +2,12 @@
 
 int main(void) {
 
+    printf("===^^===\n");
+    printf("Welcome to the Decimal to Hexadecimal Converter!\n");
+    printf("Enter a number:\n");
+
+    int the_number; // declares an integer variable
+    scanf("%d", &the_number); // acquires user input
+
     return 0;
 }
